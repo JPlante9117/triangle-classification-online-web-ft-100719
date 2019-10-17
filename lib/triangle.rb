68deddd5189f4228636326ab5153c47a147047c2
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-17 18:11:03 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
     @side1 = side1
     @side2 = side2
     @side3 = side3
-    @all_sides = [@side1, @side2, @side3]
+    @all_sides = [@side1, @side2, @side3].sort
   end
   
   def is_equilateral?
