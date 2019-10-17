7f4ad9c901ac39bf8aff39f59f1a17bebb755390
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-17 17:54:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,8 +6,45 @@ class Triangle
     
   end
   
+  def is_equilateral?
+    @side1 == @side2 && @side2 == @side3 ? true : false
+  end
+  
+  def is_isoceles?
+    if (@side1 == @side2 && @side2 != @side3) || (@side1 == @side3 && @side3 != @side2) || (@side2 == @side3 && @side3 != @side1)
+      true
+    else
+      false
+    end
+  end
+  
+  def is_scalene?
+    if @side1 != @side2 && @side2 != @side3 && @side1 != @side3
+      true
+    else
+      false
+    end
+  end
+  
+  def is_invalid?
+    @side1 + @side2 < @side3 ? true : false
+  end
+  
   def kind
     #returns either :equilateral, :isoceles, or :scalene
+    if is_invalid?
+      
+    elsif is_equilateral?
+      :equilateral
+    elsif is_isoceles?
+      :isoceles
+    elsif is_scalene?
+      :scalene
+    end
+  end
+  
+  class TriangleError < StandardError
+    
   end
   
 end
