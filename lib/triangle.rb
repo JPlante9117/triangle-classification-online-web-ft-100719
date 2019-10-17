82d856f68d214fbd64a7d4cfba20728dd359c1b8
Jacques Robert Plante <jonlypaffle@gmail.com>
2019-10-17 17:33:02 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,11 @@
 class Triangle
-  # write code here
+  
+  def self.initialize(side1, side2, side3)
+    
+  end
+  
+  def kind
+    #returns either :equilateral, :isoceles, or :scalene
+  end
+  
 end
